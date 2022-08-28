86193b62c7570559d5ccc4babc5ff04f12d28dee
Alistair Woodcock <alistairwoodcock@gmail.com>
2022-08-29 08:15:41 +1000

    Handle block widths and counting elapsed time

--- src/main.zig
+++ src/main.zig
@@ -12,10 +12,14 @@ const Position = struct {
     y: c_int,
 };
 
-var pos = Position {
+var pos = Position{
     .x = 0,
     .y = 0,
 };
+
+const block_width = 32;
+const screen_width = 384;
+const screen_height = 704;
 
 pub fn main() !void {
     if (c.SDL_Init(c.SDL_INIT_VIDEO) != 0) {
@@ -24,13 +28,7 @@ pub fn main() !void {
     }
     defer c.SDL_Quit();
 
-    const screen = c.SDL_CreateWindow(
-        "Esching", 
-        c.SDL_WINDOWPOS_UNDEFINED, 
-        c.SDL_WINDOWPOS_UNDEFINED, 
-        1280, 720, 
-        c.SDL_WINDOW_OPENGL | c.SDL_WINDOW_RESIZABLE
-    ) orelse {
+    const screen = c.SDL_CreateWindow("Esching", c.SDL_WINDOWPOS_UNDEFINED, c.SDL_WINDOWPOS_UNDEFINED, screen_width, screen_height, c.SDL_WINDOW_OPENGL | c.SDL_WINDOW_RESIZABLE) orelse {
         c.SDL_Log("Unable to create window: %s", c.SDL_GetError());
         return error.SDLInitializationFailed;
     };
@@ -42,9 +40,25 @@ pub fn main() !void {
     };
     defer c.SDL_DestroyRenderer(renderer);
 
+    var prevTime: u32 = 0;
+    var secondsCount: u32 = 0;
 
     var quit = false;
     while (!quit) {
+        var currTime = c.SDL_GetTicks();
+
+        var elapsedTime = (currTime - prevTime);
+
+        secondsCount += elapsedTime;
+
+        if (secondsCount >= 1000) {
+            print("tick. {}\n", .{secondsCount});
+            secondsCount = 0;
+
+            // move_block_down(&pos);
+        }
+
+        prevTime = currTime;
 
         var event: c.SDL_Event = undefined;
 
@@ -55,19 +69,23 @@ pub fn main() !void {
                 },
 
                 c.SDL_KEYDOWN => {
-                    switch( event.key.keysym.sym ) {
+                    switch (event.key.keysym.sym) {
                         c.SDLK_LEFT => {
-                            pos.x -= 1;
+                            if (pos.x > 0) pos.x -= block_width;
                         },
                         c.SDLK_RIGHT => {
-                            pos.x += 1;
+                            if (pos.x <= (screen_width - 2 * block_width)) pos.x += block_width;
                         },
                         c.SDLK_UP => {
-                            pos.y -= 1;
+                            // pos.y -= block_width;
                         },
                         c.SDLK_DOWN => {
-                            pos.y += 1;
-                        },  
+                            if (pos.y <= (screen_height - 2 * block_width)) pos.y += block_width;
+                        },
+                        114 => { // r
+                            pos.x = 0;
+                            pos.y = 0;
+                        },
                         else => {},
                     }
                 },
@@ -79,13 +97,15 @@ pub fn main() !void {
                 else => {},
             }
         }
-        
+
+        // print("tick: {}\n", .{elapsedTime});
+
         _ = c.SDL_SetRenderDrawColor(renderer, 96, 128, 255, 255);
         _ = c.SDL_RenderClear(renderer);
 
         _ = c.SDL_SetRenderDrawColor(renderer, 0, 0xff, 0, 0xff);
-        const rect = c.SDL_Rect{ .x=pos.x, .y=pos.y, .w=100, .h=100};
-        
+        const rect = c.SDL_Rect{ .x = pos.x, .y = pos.y, .w = block_width, .h = block_width };
+
         _ = c.SDL_RenderFillRect(renderer, &rect);
 
         c.SDL_RenderPresent(renderer);
